eca2ddd2c54ea4c76b6c975caac8bb3a5b37d715
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-01-11 12:55:05 +0300

    Added the axios.

--- src/rest.js
+++ src/rest.js
@@ -1,12 +1,12 @@
+import axios from "axios"
 import {BACKEND_URL} from "./settings";
 
 class RestApi {
 
   static getFeatures() {
     return new Promise((resolve, reject) => {
-      fetch(`${BACKEND_URL}/api/things`)
-        .then(resp => resp.json())
-        .then(features => resolve(features))
+      axios(`${BACKEND_URL}/api/things`)
+        .then(resp => resolve(resp.data))
         .catch(err => reject(err));
     });
   }
@@ -14,43 +14,21 @@ class RestApi {
   // Returns auth token on success
   static login(username, password) {
     return new Promise((resolve, reject) => {
-      this.post(`${BACKEND_URL}/auth/local`, {
+      axios.post(`${BACKEND_URL}/auth/local`, {
         email: username,
         password: password
       })
-        .then(resp => resp.json())
-        .then(data => resolve(data.token))
+        .then(resp => resolve(resp.data.token))
         .catch(err => reject(err));
     });
   }
 
   static postFeature(feature) {
-    return this.post(`${BACKEND_URL}/api/things`, {name: feature});
+    return axios.post(`${BACKEND_URL}/api/things`, {name: feature});
   }
 
   static remFeature(feature) {
-    return new Promise((resolve, reject) => {
-      fetch(`${BACKEND_URL}/api/things/${feature._id}`, {
-        method: "DELETE"
-      })
-        .then(() => resolve())
-        .catch(err => reject(err));
-    });
-  }
-
-  static post(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-        .then((resp) => resolve(resp))
-        .catch(err => reject(err));
-    });
+    return axios.delete(`${BACKEND_URL}/api/things/${feature._id}`);
   }
 
 }
