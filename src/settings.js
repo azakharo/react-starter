6a830c19f574b4e12966dae9a71076dbbf20ef67
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-01-12 15:56:02 +0300

    Changed env var name for backend URL.

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 const {
-  STARTER_BACKEND_URL
+  REACT_APP_BACKEND_URL
 } = process.env;
 
-export const BACKEND_URL = STARTER_BACKEND_URL || 'http://localhost:9000';
+export const BACKEND_URL = REACT_APP_BACKEND_URL || 'http://localhost:9000';
